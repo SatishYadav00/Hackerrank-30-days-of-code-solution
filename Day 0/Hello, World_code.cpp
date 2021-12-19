7fc2be6d3d6397c5da06502f4d2f8b20896d7bf0
Satish Yadav <70273513+SatishYadav00@users.noreply.github.com>
2021-12-19 12:36:59 +0530

    Create Hello, World_code.cpp

---
+++ Day 0/Hello, World_code.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() {
+    string str;
+    getline(cin,str);
+    
+    
+    cout<<"Hello, World."<<endl;
+    cout<<str<<endl;
+    
+
+    return 0;
+}
